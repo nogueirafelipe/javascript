e823de3bd4a1c35e80558a07cac5bc52158ea88b
Felipe Nogueira <98784270+nogueirafelipe@users.noreply.github.com>
2022-02-15 18:25:43 -0300

    Criei a aula12

---
+++ aula12/ex013.js
@@ -0,0 +1,16 @@
+var agora = new Date()
+var hora = agora.getHours()
+
+console.log(`Agora são ${hora} horas.`)
+
+if (hora < 12 && hora >= 6) {
+    console.log(`Bom Dia!`)
+} else if (hora <= 18 && hora >= 12) {
+    console.log(`Boa Tarde!`)
+} else if (hora > 18 && hora <= 24) {
+    console.log(`Boa Noite!`)
+} else if (hora == 00) {
+    console.log(`Boa Noite!`)
+} else {
+    console.log(`Boa Madrugada!`)
+}
