24f6bd6d3dc337eeff9fe9b91451cf2ef915535b
Felipe Nogueira <98784270+nogueirafelipe@users.noreply.github.com>
2022-02-21 17:59:58 -0300

    Atualizei ex018b

--- aula14ex/ex018b/script/script.js
+++ aula14ex/ex018b/script/script.js
@@ -9,7 +9,7 @@ function calcular() {
         for (let c = 1 ; c <= 10 ; c++) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
-            item.value = `tab${c}`
+            item.value = `tab${c}` 
             tab.appendChild(item)
         }
     }
