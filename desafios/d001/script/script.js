a3223d3c3d904728d85054039cebb109bbddcdc0
Felipe Nogueira <98784270+nogueirafelipe@users.noreply.github.com>
2022-03-01 22:06:48 -0300

    Criei a pasta desafios

---
+++ desafios/d001/script/script.js
@@ -0,0 +1,15 @@
+function clicar() {
+    let botaoUm = document.getElementById('botao1')
+    alert('Você clicou no 1° botão!')
+
+}
+
+function clicou() {
+    let botaoDois = document.getElementById('botao2')
+    alert('Você clicou no 2° botão!')
+}
+
+function clique() {
+    let botaoTres = document.getElementById('botao3')
+    alert('Você clicou no 3° botão!')
+}
