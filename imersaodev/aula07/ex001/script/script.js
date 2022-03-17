c37d3877b2b9938afa54056e75fd3fc2d513dd2e
Felipe Nogueira <98784270+nogueirafelipe@users.noreply.github.com>
2022-03-17 14:35:00 -0300

    Atualizei aula07/ex001

--- imersaodev/aula07/ex001/script/script.js
+++ imersaodev/aula07/ex001/script/script.js
@@ -1,38 +1,38 @@
 var cartas = [
     carta1 = {
         nome: "Rogerinho do Ingá",
+        imagem: "https://i1.sndcdn.com/artworks-000371899050-m32dfo-t500x500.jpg",
         atributos: {
             carisma: 5,
             velocidade: 10,
             soco: 8,
-            conhecimentos: 6,
         },
     },
     carta2 = {
         nome: "Maurílio dos Anjos",
+        imagem: "https://pbs.twimg.com/media/Eae1c6RWsAEtOgH.jpg",
         atributos: {
             carisma: 10,
             velocidade: 7,
             soco: 5,
-            conhecimentos: 9,
         },
     },
     carta3 = {
         nome: "Julinho da Van",
+        imagem: "https://i1.sndcdn.com/artworks-000447180843-soockj-t500x500.jpg",
         atributos: {
             carisma: 8,
             velocidade: 9,
             soco: 9,
-            conhecimentos: 6,
         },
     },
     carta4 = {
         nome: "Renan",
+        imagem: "https://m.media-amazon.com/images/M/MV5BN2VkYTAwODEtYzlkYS00MDMwLWE5MjktZWNlMDM3NzJhYTZmXkEyXkFqcGdeQXVyNjcwMzEzMTU@._V1_.jpg",
         atributos: {
             carisma: 9,
             velocidade: 8,
             soco: 6,
-            conhecimentos: 9,
         },
     }
 ]
@@ -57,17 +57,7 @@ function sortearCarta() {
     document.getElementById("btnSortear").disabled = true
     document.getElementById("btnJogar").disabled = false
 
-    exibirOpcoes()
-}
-
-function exibirOpcoes() {
-    var opcoes = document.getElementById("opcoes")
-    var opcoesTexto = ""
-
-    for (var atributo in cartaJogador.atributos) {
-        opcoesTexto += ` <input type="radio" name="atributo" value="${atributo}" checked>` + atributo
-    }
-    opcoes.innerHTML = opcoesTexto
+    exibirCartaJogador()
 }
 
 function obtemAtributo() {
@@ -86,12 +76,49 @@ function jogar() {
     var valorCartaMaquina = cartaMaquina.atributos[atributoSelecionado]
 
     if (valorCartaJogador > valorCartaMaquina) {
-        elementoResultado.innerHTML = `Você venceu!`
+        var resultadoFinal = `<p class="resultado-final">Você venceu!</p>`
     } else if (valorCartaJogador < valorCartaMaquina) {
-        elementoResultado.innerHTML = `Você perdeu, otário!`
+        var resultadoFinal = `<p class="resultado-final">Você perdeu, otário!</p>`
     } else {
-        elementoResultado.innerHTML = `Empate!`
+        var resultadoFinal = `<p class="resultado-final">Empate!</p>`
     }
     console.log(cartaMaquina)
+    elementoResultado.innerHTML = resultadoFinal
     document.getElementById("btnJogar").disabled = true
+    exibirCartaMaquina()
+}
+
+function exibirCartaJogador() {
+  var divCartaJogador = document.getElementById("carta-jogador")
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+  
+  var moldura ='<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  
+  var tagHtml = "<div id='opcoes' class='carta-status'>"
+  
+  var opcoesTexto = ""
+
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += `<input type="radio" name="atributo" value="${atributo}" checked>` + atributo + ` ${cartaJogador.atributos[atributo]}<br>`
+    }
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+  
+  divCartaJogador.innerHTML = moldura + nome + tagHtml + opcoesTexto + `</div>`
+  
+}
+
+function exibirCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina")
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`
+  
+  var moldura ='<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  
+  var tagHtml = "<div id='opcoes' class='carta-status'>"
+  
+  var opcoesTexto = ""
+  for (var atributo in cartaMaquina.atributos) {
+    opcoesTexto += `<p type="text" name="atributo" value="${atributo}" checked>` + atributo + ` ${cartaMaquina.atributos[atributo]}</p>`
+  }
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+  divCartaMaquina.innerHTML = moldura + nome + tagHtml + opcoesTexto + `</div>`
 }
