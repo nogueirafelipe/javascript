16634c1f1a162197b13125dcba1b8fb0773d67e6
Felipe Nogueira <98784270+nogueirafelipe@users.noreply.github.com>
2022-03-26 14:31:52 -0300

    Atualizei desafiobasico2.js

--- desafios-instagram/desafiobasico2.js
+++ desafios-instagram/desafiobasico2.js
@@ -2,5 +2,9 @@ function teste(a) {
     return typeof a
 }
 console.log(teste("5"))
+console.log(teste(1))
+console.log(teste(true))
+console.log(teste([]))
+console.log(teste())
 
 //O operador typeof não precisa de parênteses
