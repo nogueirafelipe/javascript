59b2aadbe689d7e64390d036b2447c0630d35f0b
Felipe Nogueira <98784270+nogueirafelipe@users.noreply.github.com>
2022-03-26 14:22:48 -0300

    Criei desafiobasico2.js

---
+++ desafios-instagram/desafiobasico2.js
@@ -0,0 +1,16 @@
+// 1ª maneira
+// function teste(a, b) {
+//     if (a === b) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+// console.log(teste(1, 1))
+
+
+// 2ª maneira
+function teste(a, b) {
+    return a === b ? true : false
+}
+console.log(teste(10, 10))
